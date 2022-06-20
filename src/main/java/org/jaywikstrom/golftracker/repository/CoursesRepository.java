3e52443af4c340800ea6e8cc46b257d6050d72c1
Jay Wikstrom <jaywikstrom@yahoo.com>
2022-06-20 17:53:30 -0500

    Add sort to course name dropdown list

--- src/main/java/org/jaywikstrom/golftracker/repository/CoursesRepository.java
+++ src/main/java/org/jaywikstrom/golftracker/repository/CoursesRepository.java
@@ -1,9 +1,14 @@
 package org.jaywikstrom.golftracker.repository;
 
+
 import org.jaywikstrom.golftracker.model.Courses;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+    List<Courses> findAllByOrderByCourseName();
 }
--- src/main/java/org/jaywikstrom/golftracker/service/CoursesService.java
+++ src/main/java/org/jaywikstrom/golftracker/service/CoursesService.java
@@ -3,6 +3,7 @@ package org.jaywikstrom.golftracker.service;
 import org.jaywikstrom.golftracker.model.Courses;
 import org.jaywikstrom.golftracker.repository.CoursesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,9 +13,14 @@ public class CoursesService {
     @Autowired
     private CoursesRepository coursesRepository;
 
+//    public List<Courses> listAll(){
+//        return (List<Courses>) coursesRepository.findAll();
+//    }
     public List<Courses> listAll(){
-        return (List<Courses>) coursesRepository.findAll();
+        return (List<Courses>) coursesRepository.findAllByOrderByCourseName();
     }
+
+
 
     public void save(Courses courses){
         coursesRepository.save(courses);
